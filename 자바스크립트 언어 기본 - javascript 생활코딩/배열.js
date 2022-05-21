b1ed34046ed8890cf84b56668e3ce16b9cd96ed6
changwoon2 <changwoon4@naver.com>
2022-05-22 01:17:43 +0900

    첫강의시작파일

---
+++ 자바스크립트 언어 기본 - javascript 생활코딩/배열.js
@@ -0,0 +1,13 @@
+// let top = ['code','dex','kane'];
+// console.log(top);
+// console.log(top[1]);
+
+function get_members() {
+    return ["egoing","k8805","sori","jarune","cold","stone"];
+}
+
+members = get_members();
+
+for (let i = 0; i < members.length; i++) {
+    console.log(members[i].toUpperCase()); 
+}
