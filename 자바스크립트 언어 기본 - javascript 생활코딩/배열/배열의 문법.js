ccc473bfe7b80ac45d173db0a3483db2947522c1
changwoon2 <changwoon4@naver.com>
2022-05-22 16:45:27 +0900

    save

---
+++ 자바스크립트 언어 기본 - javascript 생활코딩/배열/배열의 문법.js
@@ -0,0 +1,7 @@
+var name = "egoing";
+console.log(name);
+
+var member = ["egoing", "k8805", "sorialgi"];
+console.log(member[0]);
+console.log(member[1]);
+console.log(member[2]);
---
+++ 자바스크립트 언어 기본 - javascript 생활코딩/배열/배열의 효용.js
@@ -0,0 +1,26 @@
+function get_members1() {
+  return "egoing";
+}
+
+console.log(get_members1());
+
+function get_members2() {
+  return "k8805";
+}
+
+console.log(get_members2());
+
+function get_members3() {
+  return "sorialgi";
+}
+
+console.log(get_members3());
+
+function get_members() {
+  return ["egoing", "k8805", "sorialgi"];
+}
+
+var members = get_members();
+console.log(members[0]);
+console.log(members[1]);
+console.log(members[2]);
