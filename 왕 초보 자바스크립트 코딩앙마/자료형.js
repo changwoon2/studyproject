1ffc07dba283060f55f6834cf10d836d0388cad0
changwoon2 <changwoon4@naver.com>
2022-06-06 23:43:49 +0900

    초보 자바스크립트

---
+++ 왕 초보 자바스크립트 코딩앙마/자료형.js
@@ -0,0 +1,48 @@
+const name = "mike";
+const age = 30;
+const name1 = "mike";
+const name2 = "mike";
+const name3 = "mike";
+
+const message = "I'm a boy.";
+const message2 = "I'm a boy.";
+const message3 = "My name is ${name}";
+const message4 = "나는 ${30+1}살 입니다.";
+console.log(message4);
+
+// const age = 30;
+const PI = 3.14;
+
+console.log(1 + 2);
+console.log(10 - 3);
+console.log(3 * 2);
+console.log(6 / 3);
+console.log(6 % 4);
+
+const x = 1 / 0;
+console.log(x);
+
+const mike = "mike";
+const y = name / 2;
+
+console.log(y);
+
+const a = "true";
+const b = "false";
+
+const fat = "app";
+const s20 = 30;
+
+console.log(name == "mike");
+console.log(age > 40);
+
+let user = null;
+
+const news = "mike";
+
+console.log(typeof 3);
+console.log(typeof name);
+console.log(typeof true);
+console.log(typeof "xxx");
+console.log(typeof null);
+console.log(typeof undefined);
