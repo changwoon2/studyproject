c1420118536f6e27d57acaf4d972f22860df1316
changwoon2 <changwoon4@naver.com>
2022-06-23 23:20:27 +0900

    자바스크립트 언어 기본

---
+++ 자바스크립트 언어 기본 - javascript 생활코딩/유효범위/유효범위의 효용성.js
@@ -0,0 +1,8 @@
+function a() {
+  var i = 0;
+  i = 0;
+}
+for (var i = 0; i < 5; i++) {
+  a();
+  console.log(i);
+}
---
+++ 자바스크립트 언어 기본 - javascript 생활코딩/유효범위/전역변수 사용하는법.js
@@ -0,0 +1,38 @@
+// 전역변수
+
+// MYAPP = {}
+// MYAPP.calculator = {
+//     'left' : null,
+//     'right' : null
+// }
+// MYAPP.coordinate = {
+//     'left' : null,
+//     'right' : null
+// }
+
+// MYAPP.calculator.left = 10;
+// MYAPP.calculator.right = 20;
+// function sum(){
+//     return MYAPP.calculator.left + MYAPP.calculator.right;
+// }
+// console.log(sum());
+
+// 익명함수
+
+(function () {
+  var MYAPP = {};
+  MYAPP.calculator = {
+    left: null,
+    right: null,
+  };
+  MYAPP.coordinate = {
+    left: null,
+    right: null,
+  };
+  MYAPP.calculator.left = 10;
+  MYAPP.calculator.right = 20;
+  function sum() {
+    return MYAPP.calculator.left + MYAPP.calculator.right;
+  }
+  console.log(sum());
+})();
